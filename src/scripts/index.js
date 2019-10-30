5d65060f55e01a2d4e8e4ae2c0d359369d76b1b7
Daniel <daniel.holbling@gmail.com>
2019-10-30 16:53:44 +0100

    Some scripting, a lot of html

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,28 @@
 import '../styles/index.scss';
 
+class Note {
+    constructor() {
+
+    }
+
+    render() {
+        const wrapper = document.querySelector('.notes');
+        
+    }
+}
+
+function init(){
+    const gridToggle = document.querySelector('#grid-toggle');
+    const addBtn = document.querySelector('#add');
+    gridToggle.addEventListener('click', function(){
+        let notes = document.querySelectorAll('.note');
+        for(let note of notes){
+            note.classList.toggle("note-list");
+        }
+    });
+    addBtn.addEventListener('click', function(){
+
+    });
+}
+
+init();
