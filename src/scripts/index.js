ca0010e46372a90b25d78932244a1ec6b9205939
Daniel Hölbling <29107418+iths-daniel-holbling@users.noreply.github.com>
2019-10-30 20:46:40 +0100

    Added deletebutton (with problems)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,30 +1,40 @@
 import '../styles/index.scss';
-
 let notes = [];
 
 class Note {
 
-    constructor(text, color) {
+    constructor(text, color, notenumber) {
         this.text = text;
         this.color = color;
+        this.notenumber = notenumber;
     }
 
     markup() {
         return `
             <div class="note-buttons">
-                <i class="material-icons md-dark">edit</i>
-                <i class="material-icons md-dark">colorize</i>
-
+                <i class="material-icons md-dark edit">edit</i>
+                <i class="material-icons md-dark color">colorize</i>
+                <i class="material-icons md-dark delete">delete</i>
             </div>
             <textarea>${this.text}</textarea>
         `;
     }
 }
+function createEventListeners(notenumber){
+    let noteObj = notes[notenumber];
+    let noteElem = document.querySelectorAll('.note')[notenumber];
+    let delNote = noteElem.querySelector('.delete');
+    delNote.addEventListener('click', function(){
+        notes.splice(notenumber, 1);
+        noteElem.remove();
+        
+    });
+}
 
 function init(){
+    
     const gridToggle = document.querySelector('#grid-toggle');
     const addBtn = document.querySelector('#add');
-    let noteArr = [];
     gridToggle.addEventListener('click', function(){
         let notes = document.querySelectorAll('.note');
         for(let note of notes){
@@ -32,15 +42,15 @@ function init(){
         }
     });
     addBtn.addEventListener('click', function(){
-        let newNote = new Note("Edit this waaaaah","rgba(255,255,0,0.5)");
+        let newNote = new Note("Edit this waaaaah","rgba(255,255,0,0.5)",notes.length);
         let notesDiv = document.querySelector(".notes");
         let noteMarkup = document.createElement('div');
         noteMarkup.className = "note";
+        noteMarkup.setAttribute('notenumber',newNote.notenumber);
         noteMarkup.innerHTML = newNote.markup();
         notesDiv.appendChild(noteMarkup);
-        noteArr.push(newNote);
-        console.log(noteArr);
-
+        notes.push(newNote);
+        createEventListeners(newNote.notenumber);
     });
 }
 
