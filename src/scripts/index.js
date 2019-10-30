d34c8f4f6237f3fe75a0197a452d14f4ca4c9fed
Daniel Hölbling <29107418+iths-daniel-holbling@users.noreply.github.com>
2019-10-30 18:53:01 +0100

    Add new note

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,19 +1,30 @@
 import '../styles/index.scss';
 
-class Note {
-    constructor() {
+let notes = [];
 
+class Note {
+
+    constructor(text, color) {
+        this.text = text;
+        this.color = color;
     }
 
-    render() {
-        const wrapper = document.querySelector('.notes');
-        
+    markup() {
+        return `
+            <div class="note-buttons">
+                <i class="material-icons md-dark">edit</i>
+                <i class="material-icons md-dark">colorize</i>
+
+            </div>
+            <textarea>${this.text}</textarea>
+        `;
     }
 }
 
 function init(){
     const gridToggle = document.querySelector('#grid-toggle');
     const addBtn = document.querySelector('#add');
+    let noteArr = [];
     gridToggle.addEventListener('click', function(){
         let notes = document.querySelectorAll('.note');
         for(let note of notes){
@@ -21,6 +32,14 @@ function init(){
         }
     });
     addBtn.addEventListener('click', function(){
+        let newNote = new Note("Edit this waaaaah","rgba(255,255,0,0.5)");
+        let notesDiv = document.querySelector(".notes");
+        let noteMarkup = document.createElement('div');
+        noteMarkup.className = "note";
+        noteMarkup.innerHTML = newNote.markup();
+        notesDiv.appendChild(noteMarkup);
+        noteArr.push(newNote);
+        console.log(noteArr);
 
     });
 }
