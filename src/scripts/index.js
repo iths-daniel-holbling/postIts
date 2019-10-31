a58ae009bea8c1f6716ebb306d4c8ee763c7815b
Daniel Hölbling <29107418+iths-daniel-holbling@users.noreply.github.com>
2019-10-31 11:23:50 +0100

    Debugging and added color changer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,44 +3,70 @@ let notes = [];
 let jsonNotes = "";
 
 class Note {
-
     constructor(text, color, notenumber) {
         this.text = text;
         this.color = color;
         this.notenumber = notenumber;
     }
 
+    // markup() {
+    //     return `
+    //         <div class="note-buttons">
+    //             <i class="material-icons md-dark edit">edit</i>
+    //             <i class="material-icons md-dark color">colorize</i>
+    //             <i class="material-icons md-dark delete">delete</i>
+    //         </div>
+    //         <textarea>${this.text}</textarea>
+    //     `;
+    // }
+
     markup() {
         return `
             <div class="note-buttons">
-                <i class="material-icons md-dark edit">edit</i>
-                <i class="material-icons md-dark color">colorize</i>
+                <input type="color" class="colorpicker" name="colorpicker" value="${this.color}">
                 <i class="material-icons md-dark delete">delete</i>
             </div>
             <textarea>${this.text}</textarea>
         `;
     }
 }
+
+
+
 function createEventListeners(notenumber){
-    let noteObj = notes[notenumber];
-    let noteElem = document.querySelectorAll('.note')[notenumber];
+    let index = notes.findIndex(x => x.notenumber === notenumber);
+    let noteObj = notes[index];
+    let noteElem = document.querySelectorAll('.note')[index];
     let delNote = noteElem.querySelector('.delete');
     let textArea = noteElem.querySelector('textarea');
+    let colorPicker = noteElem.querySelector('input');
+
     delNote.addEventListener('click', function(){
         notes = notes.filter(function(obj) {
             return obj.notenumber !== notenumber;
         });
         noteElem.remove();
         console.log(notes);
+        saveNotes();
     });
+
     textArea.addEventListener('keyup',function(){
         noteObj.text = textArea.value;
         saveNotes();
     });
+
+    colorPicker.addEventListener('change', function (){
+        noteObj.color = colorPicker.value;
+        noteElem.style = `background-color: ${noteObj.color};`;
+        saveNotes();
+    });
 }
 
+
+
 function loadSavedNotes(){
-    if(localStorage.notes !== 'undefined'){
+    console.log(localStorage.notes);
+    if(typeof localStorage.notes !== 'undefined'){
         let parsedNotes = JSON.parse(localStorage.notes);
         console.log(parsedNotes);
         notes = parsedNotes;
@@ -50,10 +76,10 @@ function loadSavedNotes(){
             let noteMarkup = document.createElement('div');
             noteMarkup.className = "note";
             noteMarkup.setAttribute('notenumber',newNote.notenumber);
+            noteMarkup.style = `background-color: ${newNote.color};`;
             noteMarkup.innerHTML = `
             <div class="note-buttons">
-                <i class="material-icons md-dark edit">edit</i>
-                <i class="material-icons md-dark color">colorize</i>
+                <input type="color" class="colorpicker" name="colorpicker" value="${newNote.color}">
                 <i class="material-icons md-dark delete">delete</i>
             </div>
             <textarea>${newNote.text}</textarea>
@@ -64,10 +90,21 @@ function loadSavedNotes(){
         }
     }
 }
+
+
+
 function saveNotes(){
     jsonNotes = JSON.stringify(notes);
     localStorage.setItem('notes',jsonNotes);
 }
+
+
+
+function randomColor(){
+    return "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+}
+
+
 
 function init(){
     loadSavedNotes();
@@ -80,11 +117,19 @@ function init(){
         }
     });
     addBtn.addEventListener('click', function(){
-        let newNote = new Note("Edit this waaaaah","rgba(255,255,0,0.5)",notes.length);
+        console.log(notes);
+        let newNote;
+        if(notes === undefined || notes.length == 0){
+            newNote = new Note("Edit this waaaaah",randomColor(),notes.length);
+        }else{
+            newNote = new Note("Edit this waaaaah",randomColor(),(notes[notes.length-1].notenumber+1));
+        }
+        
         let notesDiv = document.querySelector(".notes");
         let noteMarkup = document.createElement('div');
         noteMarkup.className = "note";
-        noteMarkup.setAttribute('notenumber',newNote.notenumber);
+        noteMarkup.setAttribute('notenumber', newNote.notenumber);
+        noteMarkup.style = `background-color: ${newNote.color};`;
         noteMarkup.innerHTML = newNote.markup();
         notesDiv.appendChild(noteMarkup);
         notes.push(newNote);
